fb145423c2275d8e37378f305375f04c887486ba
frbarbre <fr.barbre@gmail.com>
2023-06-29 03:01:18 +0200

    dummy

--- app/[country]/page.jsx
+++ app/[country]/page.jsx
@@ -18,7 +18,7 @@ export default async function CountryPage({ params }) {
 
   const { country } = params;
   // Getting the data for the specific page using params
-  const data = await fetch(`https://restcountries.com/v3.1/name/${params}`);
+  const data = await fetch(`https://restcountries.com/v3.1/name/${country}`);
   const res = await data.json();
   const countryData = res[0];
 
