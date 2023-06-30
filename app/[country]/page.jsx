d138371404f56344088f9bbdd44024a2e888bfc3
frbarbre <fr.barbre@gmail.com>
2023-06-30 10:25:44 +0200

    server side rendering for country pages

--- app/[country]/page.jsx
+++ app/[country]/page.jsx
@@ -1,17 +1,17 @@
-"use client";
+
 
 import CountryData from "../components/CountryData";
-import { usePathname } from "next/navigation";
 
-export default async function CountryPage() {
-  const path = usePathname();
+export default async function CountryPage({params}) {
+
+const { country } = params
 
   // Getting data from the entire API
   const allData = await fetch(`https://restcountries.com/v3.1/all`);
   const allRes = await allData.json();
 
   // Getting the data for the specific page using params
-  const data = await fetch(`https://restcountries.com/v3.1/name${path}`);
+  const data = await fetch(`https://restcountries.com/v3.1/name/${country}`);
   const res = await data.json();
   const countryData = res[0];
 
